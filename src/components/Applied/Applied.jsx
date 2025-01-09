2a40186a44b7af561e6cecfb0f8e9ba29eaebfa3
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-01-10 01:53:26 +0600

    Filter done

--- src/components/Applied/Applied.jsx
+++ src/components/Applied/Applied.jsx
@@ -7,31 +7,54 @@ const Applied = () => {
     const jobs = useLoaderData()
 
     const[appliedJobs, setAppliedJobs] = useState([])
+    const[displayJobs, setDisplayJobs] = useState([])
 
     useEffect(() => {
         const storedJobIds = getStoredJobApplication()
         if(jobs.length > 0) {
             const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id))
             setAppliedJobs(jobsApplied)
+            setDisplayJobs(jobsApplied)
         }
     },[])
+
+    const handleFilter = filter => {
+        if(filter === 'all') {
+            setDisplayJobs(appliedJobs)
+        }
+        else if (filter === 'remote') {
+            const remoteJobs = appliedJobs.filter(jobs => jobs.remote_or_onsite === 'Remote') 
+            setDisplayJobs(remoteJobs)
+        }
+        else if (filter === 'onsite') {
+            const onlineJobs = appliedJobs.filter(jobs => jobs.remote_or_onsite === 'Onsite') 
+            setDisplayJobs(onlineJobs)
+        }
+        else if (filter === 'fullTime') {
+            const fullTimeJobs = appliedJobs.filter(jobs => jobs.remote_or_onsite === 'Full Time') 
+            setDisplayJobs(fullTimeJobs)
+        }
+    }
 
     return (
         <div>
             <p className="text-4xl font-bold my-7 text-center">Applied Jobs: {appliedJobs.length}</p>
             <div>
-                <div>
-                    <select className="select w-full max-w-xs">
-                        <option disabled selected>Filter By</option>
-                        <option>All</option>
-                        <option>Remote</option>
-                        <option>Full Time</option>
-                        <option>Onsite</option>
+                <div className="flex my-3 justify-end">
+                    <select 
+                        className="select bg-slate-100"
+                        onChange={(e) => handleFilter(e.target.value)}
+                    >
+                        <option disabled hidden selected>Filter By</option>
+                        <option value="all">All</option>
+                        <option value="remote">Remote</option>
+                        <option value="fullTime">Full Time</option>
+                        <option value="onsite">Onsite</option>
                     </select>
                 </div>
                 <div className="flex flex-col gap-3 mb-10">
                     {
-                        appliedJobs.map(job => <AppliedJobsCard
+                        displayJobs.map(job => <AppliedJobsCard
                             key={job.id}
                             job={job}
                         ></AppliedJobsCard>)
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
 
@@ -33,7 +33,8 @@ const Header = () => {
                         {links}
                 </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">CareerHub</a>
+                <Link to={'/'} className="btn btn-ghost career-hub-title text-2xl">CareerHub
+                </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
